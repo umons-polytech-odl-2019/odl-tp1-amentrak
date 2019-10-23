a9ac65da6f2e324925b641abff54ef28bf258c1b
181474 <zowi.valvekens@student.umons.ac.be>
2019-10-23 15:21:08 +0200

    je suis le boss

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,23 @@
 package exercise2;
-
+import java.util.*;
 class Person {
+	private int age;
+private static int totalAge = 0;
+private static int populationSize = 0 ;
+
+public Person(int age){
+	this.age = age;
+	totalAge += this.age;
+	populationSize ++;
+}
+public static int computePopulationSize(){
+	return populationSize;
+}
+public static float computeAveragePopulationAge(){
+	return totalAge/populationSize;
+}
+public static void resetPopulation(){
+	totalAge=0;
+	populationSize=0;
+}
 }
